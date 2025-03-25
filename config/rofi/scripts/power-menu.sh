6bb06915513790c11486a6e9d42fef31018bb1c3
Alexander Popov <alexander.popov233@gmail.com>
2025-03-26 00:17:37 +0100

    power menu

--- config/rofi/scripts/power-menu.sh
+++ config/rofi/scripts/power-menu.sh
@@ -1,13 +1,13 @@
 #!/bin/bash
 
 # CMDs
-uptime="$(uptime | cut -d 's/up' -f1)"
+uptime="$(uptime | cut -d ',' -f1 | cut -d ' ' -f6)"
 host=$(hostname)
 
-shutdown=""
-suspend=""
-reboot=""
-logout=""
+shutdown=" "
+suspend=" "
+reboot=" "
+logout=" "
 
 chosen=$(
   echo -e "$shutdown\n$suspend\n$reboot\n$logout" | rofi -dmenu \
